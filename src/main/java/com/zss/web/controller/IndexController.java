e3a5fcf136ddb70822117689f9858111d175a7ab
zss_376987715 <376987715@qq.com>
2021-05-31 15:40:37 +0800

    fix

--- src/main/java/com/zss/web/controller/IndexController.java
+++ src/main/java/com/zss/web/controller/IndexController.java
@@ -1,15 +1,14 @@
 package com.zss.web.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.zss.web.domain.AuthParam;
 import com.zss.web.domain.StreamParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class IndexController {
-
 
     @RequestMapping("index/json")
     public String jsonRequest(@RequestBody StreamParam streamParam){
